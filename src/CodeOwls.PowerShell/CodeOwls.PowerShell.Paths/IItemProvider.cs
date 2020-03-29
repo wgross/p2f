a1d8d2c84ebfe836a3b8e363b8fed95b915100ea
Wolfgang Gross <github@text-mode.de>
2020-03-29 12:24:03 +0200

    Project changed to netstandard2,0

--- src/CodeOwls.PowerShell/CodeOwls.PowerShell.Paths/IItemProvider.cs
+++ src/CodeOwls.PowerShell/CodeOwls.PowerShell.Paths/IItemProvider.cs
@@ -34,55 +34,55 @@ namespace CodeOwls.PowerShell.Paths
     /// The item is provided completely fro <see cref="GetItem"/> or as properties collection from
     /// <see cref="GetItemProperties(IEnumerable{string})"/>
     /// </summary>
-    public interface IItemProvider : IGetItem
-    {
-        IEnumerable<PSPropertyInfo> GetItemProperties(IProviderContext providerContext, IEnumerable<string> propertyNames) => GetItemProperties(providerContext, this, propertyNames);
+    //public interface IItemProvider : IGetItem
+    //{
+    //    IEnumerable<PSPropertyInfo> GetItemProperties(IProviderContext providerContext, IEnumerable<string> propertyNames) => GetItemProperties(providerContext, this, propertyNames);
 
-        #region GetItemProperties default implementation
+    //    #region GetItemProperties default implementation
 
-        static IEnumerable<PSPropertyInfo> GetItemProperties(IProviderContext providerContext, IItemProvider thisPathValue, IEnumerable<string> propertyNames)
-        {
-            if (propertyNames is null)
-            {
-                yield break;
-            }
+    //    static IEnumerable<PSPropertyInfo> GetItemProperties(IProviderContext providerContext, IItemProvider thisPathValue, IEnumerable<string> propertyNames)
+    //    {
+    //        if (propertyNames is null)
+    //        {
+    //            yield break;
+    //        }
 
-            var thisItem = thisPathValue.GetItem(providerContext);
-            var propDescs = TypeDescriptor.GetProperties(thisItem);
-            var props = (from PropertyDescriptor prop in propDescs
-                         where (propertyNames.Contains(prop.Name, StringComparer.InvariantCultureIgnoreCase))
-                         select prop);
+    //        var thisItem = thisPathValue.GetItem(providerContext);
+    //        var propDescs = TypeDescriptor.GetProperties(thisItem);
+    //        var props = (from PropertyDescriptor prop in propDescs
+    //                     where (propertyNames.Contains(prop.Name, StringComparer.InvariantCultureIgnoreCase))
+    //                     select prop);
 
-            foreach (var p in props)
-            {
-                var iv = p.GetValue(thisItem);
-                if (null != iv)
-                {
-                    yield return new PSNoteProperty(p.Name, iv);
-                }
-            };
-        }
+    //        foreach (var p in props)
+    //        {
+    //            var iv = p.GetValue(thisItem);
+    //            if (null != iv)
+    //            {
+    //                yield return new PSNoteProperty(p.Name, iv);
+    //            }
+    //        };
+    //    }
 
-        #endregion GetItemProperties default implementation
+    //    #endregion GetItemProperties default implementation
 
-        void SetItemProperties(IProviderContext providerContext, IEnumerable<PSPropertyInfo> properties) => SetItemProperties(providerContext, this, properties);
+    //    void SetItemProperties(IProviderContext providerContext, IEnumerable<PSPropertyInfo> properties) => SetItemProperties(providerContext, this, properties);
 
-        #region SetItemProperties default implementation
+    //    #region SetItemProperties default implementation
 
-        static void SetItemProperties(IProviderContext providerContext, IItemProvider thisPathValue, IEnumerable<PSPropertyInfo> properties)
-        {
-            var nodeItem = thisPathValue.GetItem(providerContext);
-            var propDescs = TypeDescriptor.GetProperties(nodeItem);
-            var props = (from PropertyDescriptor propDesc in propDescs
-                         let psod = (from pso in properties
-                                     where StringComparer.InvariantCultureIgnoreCase.Equals(pso.Name, propDesc.Name)
-                                     select pso).FirstOrDefault()
-                         where null != psod
-                         select new { PSProperty = psod, Property = propDesc });
+    //    static void SetItemProperties(IProviderContext providerContext, IItemProvider thisPathValue, IEnumerable<PSPropertyInfo> properties)
+    //    {
+    //        var nodeItem = thisPathValue.GetItem(providerContext);
+    //        var propDescs = TypeDescriptor.GetProperties(nodeItem);
+    //        var props = (from PropertyDescriptor propDesc in propDescs
+    //                     let psod = (from pso in properties
+    //                                 where StringComparer.InvariantCultureIgnoreCase.Equals(pso.Name, propDesc.Name)
+    //                                 select pso).FirstOrDefault()
+    //                     where null != psod
+    //                     select new { PSProperty = psod, Property = propDesc });
 
-            props.ToList().ForEach(p => p.Property.SetValue(nodeItem, p.PSProperty.Value));
-        }
+    //        props.ToList().ForEach(p => p.Property.SetValue(nodeItem, p.PSProperty.Value));
+    //    }
 
-        #endregion SetItemProperties default implementation
-    }
+    //    #endregion SetItemProperties default implementation
+    //}
 }
